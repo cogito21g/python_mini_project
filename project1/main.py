f7be2c6d09d8630536fa9df7a9d2ee6d5b8415cd
cogito21g <falcon940105@gmail.com>
2024-06-11 23:00:48 +0900

    feat: Input FileName and Number to Make Number Of Files Automatically
    branch: main
    
    - input file_name and number
    - create number of files
    - file format is file_name_{i}.md

--- project1/main.py
+++ project1/main.py
@@ -1,14 +1,24 @@
 import os
 
+# 폴더 생성 함수
+def create_directory(directory):
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
 # 파일 생성 함수
-def create_markdown_files(num_files):
+def create_markdown_files(directory, base_name, num_files):
+    create_directory(directory)
     for i in range(1, num_files + 1):
-        file_name = f"file{i}.md"
+        file_name = os.path.join(directory, f"{base_name}_{i}.md")
         with open(file_name, "w") as file:
             file.write(f"# 파일 {i}\n")
             file.write(f"이것은 파일 {i}의 내용입니다.\n")
 
-# 20개의 markdown 파일 생성
-create_markdown_files(20)
+# 사용자 입력 받기
+directory = "progress"
+base_name = input("생성할 파일의 기본 이름을 입력하세요: ")
+num_files = int(input("생성할 파일의 개수를 입력하세요: "))
 
-print("20개의 Markdown 파일이 생성되었습니다.")
+create_markdown_files(directory, base_name, num_files)
+
+print(f"{directory} 폴더에 {num_files}개의 Markdown 파일이 생성되었습니다.")
